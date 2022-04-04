134fec199807217997ecdbd23771285f555f13b6
elifozisik <elifozisik@outlook.com>
2022-04-04 15:41:52 +0300

    v1.0

---
+++ test.py
@@ -0,0 +1,27 @@
+import jetson.inference
+import jetson.utils
+import cv2
+
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold = 0.5)
+
+cap = cv2.VideoCapture('nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink', cv2.CAP_GSTREAMER)
+
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+
+    imgCuda = jetson.utils.cudaFromNumpy(frame)
+    detections = net.Detect(imgCuda)
+    frame = jetson.utils.cudaToNumpy(imgCuda)
+
+    cv2.imshow('Input', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
